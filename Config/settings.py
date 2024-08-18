a2aba0b9667e3093ee80845a657789fea2a785c4
Diego Cazon Condori <diego.xmen123@gmail.com>
2024-08-18 08:53:46 -0300

    +passwordAcces , seting/allow_urls

--- Config/settings.py
+++ Config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4(jeh4ts0)4#+411!o%a*#!wt&*p0g_(#zds&%40ofxmxlgm4%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
